914aa3bcb24bffdad4ecef19d114dfae61185a2d
Titan-004 <albusdumbledoor@gmail.com>
2025-07-30 21:27:21 +0530

    shifted frontend indpendently

--- extrabite-frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { isLoggedIn } from '../util/auth';
-
-const ProtectedRoute = () => {
-  return isLoggedIn() ? <Outlet /> : <Navigate to="/signin" replace />;
-};
-
-export default ProtectedRoute; 
